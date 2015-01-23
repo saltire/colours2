e3bfd77e2b8bb8b6ee520d21359fbf120664a50a
saltire sable <x@saltiresable.com>
2015-01-23 02:24:41 -0500

    Moved Bower deps to NPM; streamlined tests; fixed whitespace

--- spec/spec.js
+++ spec/spec.js
@@ -2,24 +2,21 @@ describe('colourCtrl', function () {
     // module and inject come from the angular.mock namespace
     beforeEach(module('colourApp'));
 
-    it('should convert RGB to hex', inject(function ($controller) {
-        var $scope = {};
-        var ctrl = $controller('colourCtrl', {$scope: $scope});
+    var $scope;
+    beforeEach(inject(function($controller) {
+        $scope = {};
+        $controller('colourCtrl', {$scope: $scope});
+    }));
 
+    it('should convert RGB to hex', function () {
         expect($scope.hex(0, 255, 0)).toBe('#00ff00');
-    }));
+    });
 
-    it('should convert RGB to HSV', inject(function ($controller) {
-        var $scope = {};
-        $controller('colourCtrl', {$scope: $scope});
-
+    it('should convert RGB to HSV', function () {
         expect($scope.rgb2hsv(179, 153, 204)).toEqual([270, 25, 80]);
-    }));
+    });
 
-    it('should convert HSV to RGB', inject(function ($controller) {
-        var $scope = {};
-        $controller('colourCtrl', {$scope: $scope});
-
+    it('should convert HSV to RGB', function () {
         expect($scope.hsv2rgb(270, 25, 80)).toEqual([178, 153, 204]);
-    }));
+    });
 });
