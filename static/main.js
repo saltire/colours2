37187cabc2663dce57d8a015d0c6eb38eb33f0c0
saltire sable <x@saltiresable.com>
2015-02-04 17:06:05 -0500

    Got hex validation working

--- static/main.js
+++ static/main.js
@@ -13,31 +13,12 @@ colourApp.controller('colourCtrl', function ($scope, $log, $http) {
 colourApp.directive('colourRow', function ($log) {
     return {
         link: function (scope, elem, attr) {
-            scope.$watch('colour.hex', function (hex) {
-                scope.colour.red = parseInt(hex.slice(1, 3), 16);
-                scope.colour.green = parseInt(hex.slice(3, 5), 16),
-                scope.colour.blue = parseInt(hex.slice(5, 7), 16);
-
-                var hsv = rgb2hsv(scope.colour.red, scope.colour.green, scope.colour.blue);
-                scope.colour.hue = hsv.h;
-                scope.colour.sat = hsv.s;
-                scope.colour.val = hsv.v;
-            });
-
             scope.$watchGroup(['colour.red', 'colour.green', 'colour.blue'], function () {
+                console.log('watch rgb');
                 var hsv = rgb2hsv(scope.colour.red, scope.colour.green, scope.colour.blue);
                 scope.colour.hue = hsv.h;
                 scope.colour.sat = hsv.s;
                 scope.colour.val = hsv.v;
-
-                scope.colour.hex = rgb2hex(scope.colour.red, scope.colour.green, scope.colour.blue);
-            });
-
-            scope.$watchGroup(['colour.hue', 'colour.sat', 'colour.val'], function () {
-                var rgb = hsv2rgb(scope.colour.hue, scope.colour.sat, scope.colour.val);
-                scope.colour.red = rgb.r;
-                scope.colour.green = rgb.g;
-                scope.colour.blue = rgb.b;
 
                 scope.colour.hex = rgb2hex(scope.colour.red, scope.colour.green, scope.colour.blue);
             });
@@ -49,15 +30,32 @@ colourApp.directive('hexColour', function ($log) {
     return {
         require: 'ngModel',
         link: function (scope, elem, attr, ngModel) {
+            // parse value on update
             ngModel.$parsers.push(function (value) {
                 return value.toUpperCase()
-                    .replace(/[^A-F0-9]/g, '')
                     .replace(/^#*/, '#')
                     .replace(/^#([A-F0-9])([A-F0-9])([A-F0-9])$/, '#$1$1$2$2$3$3');
             });
+
+            // validate parsed value on update
             ngModel.$validators.hex = function (modelValue, viewValue) {
                 return /#?[A-F0-9]{6}/.test(modelValue || viewValue);
             };
+
+            // update RGB on update, if valid
+            ngModel.$viewChangeListeners.push(function () {
+                if (ngModel.$valid) {
+                    scope.colour.red = parseInt(ngModel.$modelValue.slice(1, 3), 16);
+                    scope.colour.green = parseInt(ngModel.$modelValue.slice(3, 5), 16),
+                    scope.colour.blue = parseInt(ngModel.$modelValue.slice(5, 7), 16);
+                }
+            });
+
+            // display parsed value on unfocus
+            elem.on('change', function (e) {
+                ngModel.$setViewValue(ngModel.$modelValue);
+                ngModel.$render();
+            });
         }
     }
 });
